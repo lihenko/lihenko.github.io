f2259117821fb449d3f1f9074c59eb0c576e0a3d
lihenko <obbdpu@gmail.com>
2019-12-03 16:51:32 +0200

    EVO Plus обновлена почта заказчика + добавлен font-display: swap

--- evoplus/sendcontact.php
+++ evoplus/sendcontact.php
@@ -2,9 +2,9 @@
 $subject = $_POST['subject'];
 $number = $_POST['tel'];
 $instagram = $_POST['insta'];
-$address = 'obbdpu@gmail.com';
+$address = 'zakaztest@evo.plus';
 $mes = "Контакты \nТелефон: $number\nInstagram: $instagram \n\nОтправлено с сайта http://evo.plus";
-$headers_list = "Content-type:text/plain; charset = UTF-8\r\nFrom:EVOPlus<no-reply@lihenko.com.ua>\r\nReply-To:$address\r\nReturn-path:$address\r\nCc:hook2002@mail.ru";
+$headers_list = "Content-type:text/plain; charset = UTF-8\r\nFrom:EVOPlus<no-reply@evo.plus>\r\nReply-To:$address\r\nReturn-path:$address";
 $verify = mail ($address,$subject,$mes,$headers_list);
 
 ?>
