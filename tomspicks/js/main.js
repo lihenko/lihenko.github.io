f286522b8e827388942241224a0f77fa6dbfc989
lihenko <obbdpu@gmail.com>
2020-05-31 06:57:36 +0300

    Tomes Picks About Page added

--- tomspicks/js/main.js
+++ tomspicks/js/main.js
@@ -19,33 +19,46 @@ $('.hero-slider').slick({
 
 
 
-$('.slider-footer-content').slick({
+$('.team-slider').slick({
   autoplay:true,
   autoplaySpeed: 4000,
   adaptiveHeight: true,
   dots: false,
   arrows: false,
-  infinite: false,
+  infinite: true,
   speed: 300,
-  slidesToShow: 4,
+  slidesToShow: 6,
   slidesToScroll: 1,
+  //centerMode: true,
   responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 1
+      }
+    },
     {
       breakpoint: 991,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
+        slidesToShow: 3,
+        slidesToScroll: 1
       }
     },
     {
       breakpoint: 680,
       settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
         slidesToShow: 1,
         slidesToScroll: 1
       }
-    }
+    },
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
     // instead of a settings object
