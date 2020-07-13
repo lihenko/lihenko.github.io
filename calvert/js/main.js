c5c3bdc8cae58eaf00c80e17df72c4d901361edc
tatianalykhenko <tatiana.lykhenko@gmail.com>
2020-07-13 14:27:55 +0300

    Calvert main.js update

--- calvert/js/main.js
+++ calvert/js/main.js
@@ -1,3 +1,53 @@
+$('.clients-slider').slick({
+  autoplay:true,
+  autoplaySpeed: 4000,
+  adaptiveHeight: true,
+  dots: false,
+  arrows: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 680,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+$('.quote-slider').slick({
+  autoplay:true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+  fade: false,
+});
+
+
+
 $('.property-gallery').slick({
   autoplay:true,
   slidesToShow: 1,
