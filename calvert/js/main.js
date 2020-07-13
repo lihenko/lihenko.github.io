181422f09644b211b27ee77bda97a0739c10e447
tatianalykhenko <tatiana.lykhenko@gmail.com>
2020-07-13 13:49:22 +0300

    Calvert single property

--- calvert/js/main.js
+++ calvert/js/main.js
@@ -1,49 +1,9 @@
-$('.clients-slider').slick({
-  autoplay:true,
-  autoplaySpeed: 4000,
-  adaptiveHeight: true,
-  dots: false,
-  arrows: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 5,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 991,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-      }
-    },
-    {
-      breakpoint: 680,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
-
-
-$('.quote-slider').slick({
+$('.property-gallery').slick({
   autoplay:true,
   slidesToShow: 1,
   slidesToScroll: 1,
-  arrows: false,
+  arrows: true,
   dots: false,
-  fade: false,
 });
 
 
