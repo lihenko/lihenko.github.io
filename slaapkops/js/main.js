2886eadac9f1db8f39cda94e1ec70207344b76c4
tatianalykhenko <tatiana.lykhenko@gmail.com>
2020-08-28 19:33:34 +0300

    Slaapkops home page

---
+++ slaapkops/js/main.js
@@ -0,0 +1,153 @@
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+$('.cookie-button a').click(function () {
+  event.preventDefault();
+  setCookie('topbar', 'topbar', 1);
+  $('#bottom-bar').removeClass('open');
+});
+
+$(document).ready(function(){
+  var topbar = getCookie('topbar');
+  if (topbar == "") {
+    $('#bottom-bar').addClass('open');
+  } 
+});
+
+$('.home-slider').slick({
+    speed: 300,
+    autoplaySpeed: 4000,
+    autoplay:true,
+    arrows: true,
+    dots:false,
+    responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+      }
+    }
+  ]
+});
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+
+
+$('.slider-footer-content').slick({
+  autoplay:true,
+  autoplaySpeed: 4000,
+  adaptiveHeight: true,
+  dots: false,
+  arrows: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 680,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+$('.home-testimonials').slick({
+    speed: 300,
+    autoplay:true,
+    arrows: false,
+    dots:true
+});
+
+
+
+$('.single-slide').slick({
+  autoplay:true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+  fade: true,
+  asNavFor: '.nav-slide'
+});
+
+$('.nav-slide').slick({
+  autoplay:true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.single-slide',
+  arrows: false,
+  dots: false,
+  //centerMode: true,
+  focusOnSelect: true,
+  vertical: true,
+  verticalSwiping: true,
+  responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+        vertical: false,
+        verticalSwiping: false
+      }
+    }
+
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+
+
+
+
+
+$(document).ready(function(){
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 600);
+        return false;
+    });
+});
