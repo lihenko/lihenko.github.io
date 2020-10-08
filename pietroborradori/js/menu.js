e85e62c045c6908d7fc9c8b212bfd36fdb03ce51
tatianalykhenko <tatiana.lykhenko@gmail.com>
2020-10-08 16:41:20 +0300

    PietroBorradori

--- pietroborradori/js/menu.js
+++ pietroborradori/js/menu.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
     var button = $(".toggle-btn");
     var menu = $(".main-menu");
-    var submenu = $("#primary-menu ul");
-    var menuitem = $("#primary-menu > li");
+    var menuitem = $("#primary-menu li a");
 
     button.on("click", function(){
         $("body").toggleClass("menu-open");
@@ -10,80 +9,64 @@ $(document).ready(function(){
         menu.toggleClass("open");
     });
     
-       
+    menuitem.on("click", function(){
+        $("body").removeClass("menu-open");
+        button.removeClass("open");
+        menu.removeClass("open");
+    });  
 
+});
+
+
+
+$(document).ready(function(){
+    var listen = $('#listen').offset().top - 20;
+    var video = $('#video').offset().top - 20;
+    var contact = $('#contact').offset().top - 20;
+
+    $(window).on('scroll', function() {
+        var y_scroll_pos = window.pageYOffset;
+
+        if(y_scroll_pos > listen) {
+            $('.hamburger .icon-bar').addClass('black');
+            $('.menu-title').addClass('black');
+        } else {
+            $('.hamburger .icon-bar').removeClass('black');
+            $('.menu-title').removeClass('black');
+        }
+        if(y_scroll_pos > video){
+            $('.hamburger .icon-bar').removeClass('black');
+            $('.menu-title').removeClass('black');
+        }
+        if(y_scroll_pos > contact) {
+            $('.hamburger .icon-bar').addClass('black');
+            $('.menu-title').addClass('black');
+        }
+    });
 });
 
 $( window ).resize(function(){
-    var submenu = $("#primary-menu ul");
-    var menuitem = $("#primary-menu > li");
-    submenu.css('display', 'none');
-    menuitem.unbind('mouseover');
-    menuitem.unbind('mouseleave');
+    var listen = $('#listen').offset().top - 20;
+    var video = $('#video').offset().top -20 ;
+    var contact = $('#contact').offset().top -20 ;
 
-    if ($(window).width() > 991) {
-        
-          menuitem.mouseover( function(){
-                if ($(this).children('ul').length != 0){
+    $(window).on('scroll', function() {
+        var y_scroll_pos = window.pageYOffset;
 
-                    $(this).children('ul').stop().slideDown();
-
-                    return false;
-                }
-
-            });
-        
-
-        
-          menuitem.mouseleave( function(){
-
-                if ($(this).children('ul').length != 0){
-
-                    $(this).children('ul').stop().hide();
-                    return false;
-                }
-
-            });
-        
-
-           
-           
-    } else {
-        submenu.css('display', 'block');
-    }
-});
-
-$(document).ready(function(){
-    var submenu = $("#primary-menu ul");
-    var menuitem = $("#primary-menu > li");
-    if ($(window).width() > 991) {
-        
-          menuitem.mouseover( function(){
-                if ($(this).children('ul').length != 0){
-
-                    $(this).children('ul').stop().slideDown();
-
-                    return false;
-                }
-
-            });
-        
-
-        
-          menuitem.mouseleave( function(){
-
-                if ($(this).children('ul').length != 0){
-
-                    $(this).children('ul').stop().hide();
-                    return false;
-                }
-
-            });
-        
-
-           
-           
-    } else {
-        submenu.css('display', 'block');
-    }
+        if(y_scroll_pos > listen) {
+            $('.hamburger .icon-bar').addClass('black');
+            $('.menu-title').addClass('black');
+        } else {
+            $('.hamburger .icon-bar').removeClass('black');
+            $('.menu-title').removeClass('black');
+        }
+        if(y_scroll_pos > video){
+            $('.hamburger .icon-bar').removeClass('black');
+            $('.menu-title').removeClass('black');
+        }
+        if(y_scroll_pos > contact) {
+            $('.hamburger .icon-bar').addClass('black');
+            $('.menu-title').addClass('black');
+        }
+    });
 });
