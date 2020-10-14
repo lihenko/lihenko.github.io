096d4d55469ea189b90602c51842081934fd4e94
tatianalykhenko <tatiana.lykhenko@gmail.com>
2020-10-14 08:36:35 +0300

    PietroBorradori

--- pietroborradori/js/main.js
+++ pietroborradori/js/main.js
@@ -47,3 +47,6 @@ $(document).ready(function(){
     });
 });
 
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
