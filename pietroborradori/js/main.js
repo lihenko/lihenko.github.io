22802dfbb742546bd43d0fae6a660a6ad10e24ad
tatianalykhenko <tatiana.lykhenko@gmail.com>
2020-10-15 12:27:04 +0300

    PietroBorradori

--- pietroborradori/js/main.js
+++ pietroborradori/js/main.js
@@ -6,6 +6,29 @@ $(document).ready(function(){
       $page.animate({
             scrollTop: $('#about2').offset().top
         }, 600);
+      var listen = $('#listen').offset().top - 20;
+      var video = $('#video').offset().top -20 ;
+      var contact = $('#contact').offset().top -20 ;
+
+      $(window).on('scroll', function() {
+          var y_scroll_pos = window.pageYOffset;
+
+          if(y_scroll_pos > listen) {
+              $('.hamburger .icon-bar').addClass('black');
+              $('.menu-title').addClass('black');
+          } else {
+              $('.hamburger .icon-bar').removeClass('black');
+              $('.menu-title').removeClass('black');
+          }
+          if(y_scroll_pos > video){
+              $('.hamburger .icon-bar').removeClass('black');
+              $('.menu-title').removeClass('black');
+          }
+          if(y_scroll_pos > contact) {
+              $('.hamburger .icon-bar').addClass('black');
+              $('.menu-title').addClass('black');
+          }
+      });
     });
 });
 
