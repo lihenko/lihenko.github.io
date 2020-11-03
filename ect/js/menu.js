3aa396ae4addeca8153643d74ea16550321c0dd5
tatianalykhenko <tatiana.lykhenko@gmail.com>
2020-11-03 08:11:45 +0200

    ECT home page

---
+++ ect/js/menu.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+    var button = $(".toggle-btn");
+    var menu = $(".main-menu");
+    var submenu = $("#primary-menu ul");
+    var menuitem = $("#primary-menu > li");
+
+    button.on("click", function(){
+        $("body").toggleClass("menu-open");
+        button.toggleClass("open");
+        menu.toggleClass("open");
+    });
+    
+    $(".main-menu a").on("click", function(){
+        $("body").removeClass("menu-open");
+        button.removeClass("open");
+        menu.removeClass("open");
+    });
+       
+
+});
+
+$( window ).resize(function(){
+    var submenu = $("#primary-menu ul");
+    var menuitem = $("#primary-menu > li");
+    submenu.css('display', 'none');
+    menuitem.unbind('mouseover');
+    menuitem.unbind('mouseleave');
+
+    if ($(window).width() > 991) {
+        
+          menuitem.mouseover( function(){
+                if ($(this).children('ul').length != 0){
+
+                    $(this).children('ul').stop().slideDown();
+
+                    return false;
+                }
+
+            });
+        
+
+        
+          menuitem.mouseleave( function(){
+
+                if ($(this).children('ul').length != 0){
+
+                    $(this).children('ul').stop().hide();
+                    return false;
+                }
+
+            });
+        
+
+           
+           
+    } else {
+        submenu.css('display', 'block');
+    }
+});
+
+$(document).ready(function(){
+    var submenu = $("#primary-menu ul");
+    var menuitem = $("#primary-menu > li");
+    if ($(window).width() > 991) {
+        
+          menuitem.mouseover( function(){
+                if ($(this).children('ul').length != 0){
+
+                    $(this).children('ul').stop().slideDown();
+
+                    return false;
+                }
+
+            });
+        
+
+        
+          menuitem.mouseleave( function(){
+
+                if ($(this).children('ul').length != 0){
+
+                    $(this).children('ul').stop().hide();
+                    return false;
+                }
+
+            });
+        
+
+           
+           
+    } else {
+        submenu.css('display', 'block');
+    }
+});
