17d9920619f01686538117f64acfc1547f0370e2
tatianalykhenko <tatiana.lykhenko@gmail.com>
2020-11-14 19:56:28 +0200

    Deck Review

---
+++ deckreview/js/menu.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    var button = $(".toggle-btn");
+    var menu = $(".main-menu");
+    var socialbuttons = $(".bar-social-buttons");
+    var submenu = $("#primary-menu ul");
+    var menuitem = $("#primary-menu li a");
+
+    button.on("click", function(){
+        $("body").toggleClass("menu-open");
+        button.toggleClass("open");
+        menu.toggleClass("open");
+        socialbuttons.toggleClass("open");
+    });
+
+   menuitem.on("click", function(){
+        if ($(this).children('ul').length != 0){
+            menuitem.not(this).children('ul').slideUp();
+            $(this).children('ul').slideToggle();
+            return false;
+        }
+        $("body").removeClass("menu-open");
+        button.removeClass("open");
+        menu.removeClass("open");
+        socialbuttons.removeClass("open");
+        });
+
+});
