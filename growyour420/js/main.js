a3cbd02b66afb774e492c1f70fec77d7e22f6873
tatianalykhenko <tatiana.lykhenko@gmail.com>
2020-11-17 17:09:15 +0200

    Grow Your 420

---
+++ growyour420/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 600);
+        return false;
+    });
+});
+
+$(window).scroll(function() {
+    if ($(document).scrollTop() > 100) {
+        $('#to-top').fadeIn('slow');
+    }
+    else {
+        $('#to-top').fadeOut('slow');
+    }
+});
+
+$('#open-search').click(function () {
+    $(this).addClass('open');
+    $(this).parent('.search').addClass('open');
+    $('.menu-primary-menu-container').addClass('d-none');
+    $('#search').focus();
+});
+
+$('#close-search').click(function () {
+    $('#open-search').removeClass('open');
+    $('#open-search').parent('.search').removeClass('open');
+    $('.menu-primary-menu-container').removeClass('d-none');
+});
