08cc5de9b68f0dfdcfaf9906d1f993a5e8ec2992
tatianalykhenko <tatiana.lykhenko@gmail.com>
2021-03-31 15:41:49 +0300

    Smart City 2030

---
+++ smartcity/js/main.js
@@ -0,0 +1,99 @@
+$('#news-menu a').click(function (event) {
+  event.preventDefault();
+  $('#news-menu li').removeClass('current');
+  $(this).parent('li').addClass('current');
+  var section = '.' + $(this).data('grid');
+  $('.news-grid').hide('slow');
+  $('.events-grid').hide('slow');
+  $(section).show('slow');
+});
+
+$('.hero-slider').slick({
+    speed: 300,
+    autoplay:true,
+    arrows: true,
+    dots:true
+});
+
+
+
+$('.slider-footer-content').slick({
+  autoplay:true,
+  autoplaySpeed: 4000,
+  adaptiveHeight: true,
+  dots: false,
+  arrows: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 680,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+$('.home-testimonials').slick({
+    speed: 300,
+    autoplay:true,
+    arrows: false,
+    dots:true
+});
+
+
+
+$('.single-slide').slick({
+  autoplay:true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: false,
+  fade: true,
+  asNavFor: '.nav-slide'
+});
+
+$('.nav-slide').slick({
+  autoplay:true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.single-slide',
+  arrows: false,
+  dots: false,
+  //centerMode: true,
+  focusOnSelect: true,
+  vertical: true,
+  verticalSwiping: true,
+  responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+        vertical: false,
+        verticalSwiping: false
+      }
+    }
+
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
