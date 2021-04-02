ca8c48cd7ec6166cffd0a7688564e5e7fce1b74b
tatianalykhenko <tatiana.lykhenko@gmail.com>
2021-04-02 13:49:00 +0300

    Smart City

--- smartcity/js/main.js
+++ smartcity/js/main.js
@@ -12,6 +12,20 @@ $('#news-menu a').click(function (event) {
   }
 });
 
+$('.dropdown').click(function () {
+    $(this).attr('tabindex', 1).focus();
+    $(this).toggleClass('active');
+    $(this).find('.dropdown-menu').slideToggle(300);
+});
+$('.dropdown').focusout(function () {
+    $(this).removeClass('active');
+    $(this).find('.dropdown-menu').slideUp(300);
+});
+$('.dropdown .dropdown-menu li').click(function () {
+    $(this).parents('.dropdown').find('span').text($(this).text());
+    $(this).parents('.dropdown').find('input').attr('value', $(this).text());
+});
+
 $('.hero-slider').slick({
     speed: 300,
     autoplay:true,
