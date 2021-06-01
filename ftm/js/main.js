46b969431b6a143499810c2b420f1c717c72986c
tatianalykhenko <tatiana.lykhenko@gmail.com>
2021-06-01 17:57:40 +0300

    FTM

--- ftm/js/main.js
+++ ftm/js/main.js
@@ -1,3 +1,19 @@
+$(document).ready(function(){
+    $('.bounceIn').addClass('hidden');
+    $('.bounceIn').viewportChecker({
+      classToAdd: 'visible animate__animated animate__bounceIn',
+      offset:150
+    });
+});
+
+$(document).ready(function(){
+    $('.rubberBand').addClass('hidden');
+    $('.rubberBand').viewportChecker({
+      classToAdd: 'visible animate__animated animate__rubberBand',
+      offset:150
+    });
+});
+
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
@@ -81,92 +97,6 @@ $(document).ready(function(){
 
 
 
-$('.slider-footer-content').slick({
-  autoplay:true,
-  autoplaySpeed: 4000,
-  adaptiveHeight: true,
-  dots: false,
-  arrows: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 991,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 680,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
-
-
-$('.home-testimonials').slick({
-    speed: 300,
-    autoplay:true,
-    arrows: false,
-    dots:true
-});
-
-
-
-$('.single-slide').slick({
-  autoplay:true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  dots: false,
-  fade: true,
-  asNavFor: '.nav-slide'
-});
-
-$('.nav-slide').slick({
-  autoplay:true,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  asNavFor: '.single-slide',
-  arrows: false,
-  dots: false,
-  //centerMode: true,
-  focusOnSelect: true,
-  vertical: true,
-  verticalSwiping: true,
-  responsive: [
-    {
-      breakpoint: 991,
-      settings: {
-        slidesToShow: 3,
-        vertical: false,
-        verticalSwiping: false
-      }
-    }
-
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
-  ]
-});
-
-
-
-
-
-
-
 $(document).ready(function(){
     var $page = $('html, body');
     $('a[href*="#"]').click(function() {
@@ -176,21 +106,4 @@ $(document).ready(function(){
         return false;
     });
 });
-
-
-$('.dropdown.plan').click(function () {
-        $(this).attr('tabindex', 1).focus();
-        $(this).toggleClass('active');
-        $(this).find('.dropdown-menu').slideToggle(300);
-    });
-    $('.dropdown.plan').focusout(function () {
-        $(this).removeClass('active');
-        $(this).find('.dropdown-menu').slideUp(300);
-    });
-    $('.dropdown.plan .dropdown-menu li').click(function () {
-        $(this).parents('.dropdown').find('span').text($(this).text());
-        $(this).parents('.dropdown').find('input').attr('value', $(this).text());
-    });
-
-
 
