b586eaf26abbbc84db66e92524075c1e40d7eacc
tatianalykhenko <tatiana.lykhenko@gmail.com>
2021-11-05 17:52:43 +0200

    Manifesto

--- gyris/js/main.js
+++ gyris/js/main.js
@@ -30,3 +30,14 @@ $(document).ready(function(){
 		number.val(value + 1);
 	});
 })
+
+
+$(document).ready(function(){
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 600);
+        return false;
+    });
+});
