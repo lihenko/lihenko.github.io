1617a43ad36843c1740d606fe3735229e02ea9bb
Vladyslav <obbdpu@gmail.com>
2022-05-28 07:12:23 +0300

    Slilers

--- smilers/js/main.js
+++ smilers/js/main.js
@@ -134,3 +134,13 @@ jQuery(window).scroll(function(){
     }
   });
 });
+
+jQuery(document).ready(function($){
+    var $page = $('html, body');
+    $('a[href*="#"]').click(function() {
+        $page.animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 600);
+        return false;
+    });
+});
