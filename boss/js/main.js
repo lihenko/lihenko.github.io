5b85fe5dc53dc0c84e1f01d9fab6631a3761d701
Vladyslav <obbdpu@gmail.com>
2022-06-25 11:16:06 +0300

    Boss

--- boss/js/main.js
+++ boss/js/main.js
@@ -75,3 +75,93 @@ jQuery(window).scroll(function(){
     jQuery('#sticky-header').removeClass('sticky');
    }
 });
+
+
+function appcolumn(){
+  var appcolumn = jQuery('.application-chart-details-column');
+  var height = 1;
+  appcolumn.css('height', 'auto');
+  appcolumn.each(function(){
+    if(appcolumn.height() > height){
+      height = appcolumn.height();
+    }
+
+   });
+   appcolumn.height(height);
+}
+
+jQuery(document).ready(function () {  
+  if (jQuery(window).width() >= 992) {
+   appcolumn();
+  }
+});
+
+
+jQuery(window).resize(function() {
+    if(this.resizeTO) clearTimeout(this.resizeTO);
+    this.resizeTO = setTimeout(function() {
+        if (jQuery(window).width() >= 992) {
+         appcolumn();
+        } else {
+          jQuery('.application-chart-details-column').css('height', 'auto');
+        }
+    }, 500);
+});
+
+jQuery("#contact-form form").submit(function(e) {
+
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+
+    function isEmail(email) {
+      var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      return regex.test(email);
+    }
+
+    var form = jQuery(this);
+    var input = form.find('input');
+    var name = form.find('input[name="name"]');
+    var email = form.find('input[name="email"]');
+    var phone = form.find('input[name="phone"]');
+    var message = form.find('textarea[name="message"]');
+
+    input.css('border-color', 'transparent');
+
+
+    if (name.val() == ''){
+      name.css('border-color', 'red');
+      return;
+    }
+
+    if (email.val() == ''){
+      email.css('border-color', 'red');
+      return;
+    }
+
+    if( !isEmail(email.val())) { 
+      email.css('border-color', 'red');
+      return;
+    }
+
+    if (phone.val() == ''){
+      phone.css('border-color', 'red');
+      return;
+    }
+
+    if (message.val() == ''){
+      message.css('border-color', 'red');
+      return;
+    }
+    
+    jQuery.ajax({
+        type: "POST",
+        url: 'sendmail.php',
+        data: form.serialize(), // serializes the form's elements.
+        success: function(data)
+        {
+
+            form.html('<h3 class="text-red">Your message has been sent</h3>');
+
+        }
+    });
+    
+});
