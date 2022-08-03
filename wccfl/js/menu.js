98eb4265f9a294a227339239a14ff7f1b9d567fc
tatianalykhenko <tatiana.lykhenko@gmail.com>
2022-08-03 19:11:22 +0300

    WCCFL

--- wccfl/js/menu.js
+++ wccfl/js/menu.js
@@ -1,24 +1,33 @@
+
 jQuery(document).ready(function($) {
     let button = $('#toggle-btn');
     let menu = $('#main-menu');
     let body = $('body');
-    let windowwidth = $(window).width();
+    let html = $('html');
     let haschildren = $('#main-menu > ul > .menu-item-has-children');
-
+    
     button.click(function(event) {
         body.toggleClass('menu-open');
+        html.toggleClass('menu-open');
         button.toggleClass('open');
         menu.toggleClass('open');
     });
-
-    if(windowwidth > 991){
-      haschildren.mouseover(function(event) {
+    
+    haschildren.mouseover(function(event) {
+      let windowwidth = $(window).width();
+      if(windowwidth > 991){
         $(this).find('.sub-menu').show();
-      });
-      haschildren.mouseleave(function(event) {
+      }
+      
+    });
+    haschildren.mouseleave(function(event) {
+      let windowwidth = $(window).width();
+      if(windowwidth > 991){
         $(this).find('.sub-menu').hide();
-      });
-    }
+      }
+    });
+    
+      
 });
 
 jQuery(document).click(function(event) { 
@@ -26,8 +35,10 @@ jQuery(document).click(function(event) {
   let button = $('#toggle-btn');
   let menu = $('#main-menu');
   let body = $('body');
+  let html = $('html');
   if(!target.closest('#main-menu').length && !target.closest('#header').length){
     body.removeClass('menu-open');
+    html.removeClass('menu-open');
     button.removeClass('open');
     menu.removeClass('open');
   }       
@@ -39,6 +50,7 @@ jQuery(window).resize(function() {
   
   if(windowwidth > 991){
     submenu.hide();
+
   } else {
     submenu.show();
   }
