3b114a6cc57004740f24813c0b4f167fb82fe35e
tatianalykhenko <tatiana.lykhenko@gmail.com>
2022-10-19 15:03:30 +0300

    BOSS new

---
+++ bossnew/js/menu.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+    let button = $('#toggle-btn');
+    let menu = $('#main-menu');
+    let body = $('body');
+
+    button.click(function(event) {
+        body.toggleClass('menu-open');
+        button.toggleClass('open');
+        menu.toggleClass('open');
+    });
+});
