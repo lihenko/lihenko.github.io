1abc94cf71ee1f23d829f5749a639cd41df907cd
Vladyslav Lykhenko <obbdpu@gmail.com>
2023-11-02 18:25:24 +0200

    remove

--- joeflescher/js/main.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-    var $page = $('html, body');
-    $('a[href*="#"]').click(function() {
-        $page.animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 600);
-        return false;
-    });
-});
