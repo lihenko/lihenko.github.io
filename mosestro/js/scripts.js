1baf6a93859e01dc496dc7a9b30a221243a1cb10
lihenko <obbdpu@gmail.com>
2025-03-06 13:42:52 +0200

    Mosestro

--- mosestro/js/scripts.js
+++ mosestro/js/scripts.js
@@ -80,8 +80,8 @@ window.onscroll = function () {
 
 function scrollFunction() {
   if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
+    document.body.scrollTop > 50 ||
+    document.documentElement.scrollTop > 50
   ) {
     mybutton.style.display = "block";
   } else {
